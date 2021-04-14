bde425a2dfa5e458c3f836fbbe2ff97fb32d8210
Camille <camille.boutin1@gmail.com>
2021-04-14 18:15:44 +0200

    Inscription participant, remeber me, logout, mail réinitialisation mdp fonctionnels. Il me reste à gérer les ROLES et la gestion des messages d'erreur de validation

---
+++ src/Controller/CampusController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Form\CampusType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CampusController extends AbstractController
+{
+    /**
+     * @Route("/campus", name="campus")
+     */
+    public function createCampus(Request $request, entityManagerInterface $entityManager): Response
+    {
+        $campus = new Campus();
+        $campusForm = $this->createForm(CampusType::class, $campus);
+
+        $campusForm->handleRequest($request);
+        if ($campusForm->isSubmitted() && $campusForm->isValid()) {
+            $entityManager->persist($campus);
+            $entityManager->flush();
+        }
+        return $this->render('campus/createCampus.html.twig', ['campusForm' => $campusForm->createView()]);
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Pseudo')
+            ->add('Prenom')
+            ->add('Nom')
+            ->add('mail')
+//            ->add('agreeTerms', CheckboxType::class, [
+//                'mapped' => false,
+//                'constraints' => [
+//                    new IsTrue([
+//                        'message' => 'You should agree to our terms.',
+//                    ]),
+//                ],
+//            ])
+
+            ->add('Telephone')
+            ->add('Actif',ChoiceType::class, ['attr'=>array('required'),
+                'choices'  => [
+                    'étudiant actuel' => true,
+                    'Ancien étudiant' => false,
+                ]])
+            ->add('Campus',EntityType::class, ['class' => Campus::class,'choice_label'=>'nomCampus'
+                ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation sont différents.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe'],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
