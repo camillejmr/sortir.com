9fca485075925fc5ad7b5efe5fe04a1fb7ca7221
alex <alex.guerot@hotmail.fr>
2021-04-15 15:58:04 +0200

    suite créerSoriteType

--- src/Form/CreerSortieType.php
+++ src/Form/CreerSortieType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -10,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class CreerSortieType extends AbstractType
 {
@@ -17,26 +22,44 @@ class CreerSortieType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class, [
-                'label' => 'Nom'
+                'label' => 'Nom de la sortie'
             ])
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription', DateType::class, [
-                'html5' => true,
+            ->add('dateHeureDebut', DateTime::class, [
+                'label' => 'Date et heure de la sortie',
+                'required' => false,
                 'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'dd/mm/yyyy'
             ])
+
+            ->add('dateLimiteInscription', DateType::class, [
+                'label' => 'Date limite d inscription',
+                'required' => false,
+                'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'dd/mm/yyyy'
+            ])
+
             ->add('nombreInscriptionsMax', IntegerType::class, [
                 'label' => 'Nombre de places'
             ])
-            ->add('infosSortie', null, [
+
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée'
+            ])
+
+            ->add('infosSortie', TextType::class, [
                 'label' => 'Description et infos',
                 'required' => false
             ])
-            ->add('lieu')
-            ->add('ville', ChoiceType::class, ['attr'=> array('required')
-                
+
+            ->add('ville', EntityType::class, ['class' => Ville::class,'choice_label'=>'nomVille'
+            ])
+
+            ->add('lieu', EntityType::class, ['class' => Lieu::class,'choice_label'=>'nomLieu'
 
             ])
+
             ->add('latitude', IntegerType::class, [
                 'label' => 'latitude'
             ])
