7695c080b433bd54e96a623a7e6f4dee5bb6d7d7
alex <alex.guerot@hotmail.fr>
2021-04-15 14:16:30 +0200

    twig et form creer sortie

---
+++ src/Form/CreerSortieType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreerSortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription', DateType::class, [
+                'html5' => true,
+                'widget' => 'single_text',
+            ])
+            ->add('nombreInscriptionsMax', IntegerType::class, [
+                'label' => 'Nombre de places'
+            ])
+            ->add('infosSortie', null, [
+                'label' => 'Description et infos',
+                'required' => false
+            ])
+            ->add('lieu')
+            ->add('ville', ChoiceType::class, [
+                {{ nomVille }},
+                'multiple' => false
+            ])
+            ->add('latitude', IntegerType::class, [
+                'label' => 'latitude'
+            ])
+            ->add('longitude', IntegerType::class, [
+                'label' => 'longitude'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
