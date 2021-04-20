26f6b5621b5aac9a96dafe14a1cc38ba90d97235
kB <karloush44@hotmail.com>
2021-04-20 17:23:05 +0200

    afficher profil participants

--- src/Controller/ProfilUserController.php
+++ src/Controller/ProfilUserController.php
@@ -57,4 +57,47 @@ class ProfilUserController extends AbstractController
 
         return $this->render('profil_user/ProfilUser.html.twig', ["profilForm"=> $profilForm->createView(),'user'=>$user]);
     }
+
+    /**
+     * @Route("/profil2/{id}", name="profilParticipant_profil")
+     */
+    public function Profil2(int $id, Request $request,EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, ParticipantRepository $participantRepository): Response
+    {
+//        $user = $participantRepository->find($id);
+        $user = $entityManager->getRepository(Participant::class)->find($id);
+        if (!$user) {throw  $this->createNotFoundException('Cet utilisateur est inexistant');}
+
+
+//        $participant = new Participant();
+
+        $profilForm = $this->createForm(ProfilFormType::class, $user);
+
+        $profilForm->handleRequest($request);
+
+        if($profilForm->isSubmitted()  && $profilForm->isValid()) {
+
+
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+
+                    $profilForm->get('password')->getData()
+                )
+            );
+
+//            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do something else here
+
+            $this->addFlash('sucess', 'Profil modifié!');
+
+            return $this->redirectToRoute('main_home');
+
+
+        }
+
+        return $this->render('profil_user/ProfilParticipant.html.twig', ["profilForm"=> $profilForm->createView(),'user'=>$user]);
+    }
 }
